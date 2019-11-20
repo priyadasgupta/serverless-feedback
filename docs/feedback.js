940eedeaad8c4e636d06169ec8bc89c53faea9e7
Priyanka Dasgupta <priyanka.dasgupta@maersk.com>
2019-11-20 17:24:13 +0530

    changed the apiUrl

--- docs/feedback.js
+++ docs/feedback.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/07-feedback';
+const apiUrl = 'https://cfb1f257.eu-gb.apiconnect.appdomain.cloud/pd-feedback';
 const feedback = {
   // retrieve the existing feedback entries
   get() {
